26c0254972dc675fc5337f29576b7dbc331378c5
HalfPast9ine <116215830+HalfPast9@users.noreply.github.com>
2023-09-19 06:32:44 -0400

    GRAHHHHHHHH

---
+++ 3. Variables/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "John";
+    int characterAge = 35;
+    printf("wahhhhhhhh %s\n", characterName);
+    printf("BAHHHHHHHH %d HHHHHH\n", characterAge);
+    return 0;
+}
