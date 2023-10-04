f68022646440598f43d615e62561be7c483440f6
HalfPast9ine <116215830+HalfPast9@users.noreply.github.com>
2023-10-03 23:19:32 -0400

    BAHHHHH

--- 13. Return Statement/main.c
+++ 13. Return Statement/main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double cube(double num){
-   /*double result = num * num * num;
-   return result; */
+/* double cube(double num){
+   //double result = num * num * num;
+   return result; 
    return num * num * num;
-}
+} */
+
+// you can use prototyping to define fcutions without creating them, and you can use this to put functions below the main function. (maybe be able to completely redefine it?)
+double cube(double num);
 
 int main(){  
 
    printf("answer:%f", cube(3.0));
    return 0;
+}
+
+double cube(double num){
+   /*double result = num * num * num;
+   return result; */
+   return num * num * num;
 }
