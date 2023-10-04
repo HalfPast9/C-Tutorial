4aa6ce7faa891235ff50b3f7dc8d673d602d116d
HalfPast9ine <116215830+HalfPast9@users.noreply.github.com>
2023-10-04 15:24:09 -0400

    s

--- 21. pointers/main.c
+++ 21. pointers/main.c
@@ -4,10 +4,17 @@
 int main(){  
 
    int age = 30;
+   int * pAge = &age;
+
    double gpa = 3.4;
+   double * pGpa = &gpa;
+   
    char grade = 'A';
+   char * pGrade = &grade;
+   
 
-   printf("%p", &age);
 
+
+   // printf("%p", &age);
    return 0;
 }
