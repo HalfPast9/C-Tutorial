ec1846f5c3c7a168bcfd7789570d135d1d2c2fb6
HalfPast9ine <116215830+HalfPast9@users.noreply.github.com>
2023-10-03 23:45:02 -0400

    siwths doen

--- 14. if statements/main.c
+++ 14. if statements/main.c
@@ -6,6 +6,7 @@ int max(int num1, int num2, int num3){
    int result;
 
    //first logical operator, && is AND operator. boolean operations. || that is OR.
+   // !() negation operator: its like multiplying by -1
    if( num1 >= num2 && num1 >= num3){
       result = num1;
    }
