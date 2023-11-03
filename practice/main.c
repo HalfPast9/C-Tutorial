2e97e82c0b7b2ad22999a7d45ffb9595cc71f4f7
HalfPast9ine <116215830+HalfPast9@users.noreply.github.com>
2023-11-03 18:57:40 -0400

    BAHHH

--- practice/main.c
+++ practice/main.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int num = 6;
-    char myChar;
-    // convert int to char
-      myChar = num + '0';
-      printf("%c", myChar);
-}
 
 //Recursive practise
 
-//Iterate through the first 'n' elements an array, 
-//return the number of terget values, without the use of a loop.
+void recursive(int arr[], int target_val, int n, int *index, int *count) {
+    if (n == 0) {
+        *index = -1;  // Indicate not found
+        *count = 0;
+        return;
+    }
 
-int recursive(int arr, int target_val, int n,  int n_of_times){
-  if (n == 0){
-    return *arr
-  }
-  int smth = recursive(arr, 3, n-1);
-  if (smth == target_val){
-    n_of_times++;
-  }
-  return n_of_times
+    recursive(arr, target_val, n - 1, index, count);
+
+    if (*index == -1 && arr[n - 1] == target_val) {
+        *index = n - 1;
+        *count = 1;
+    } else if (*index != -1 && arr[n - 1] == target_val) {
+        (*count)++;
+    }
 }
 
-void main(void){
-  recursive({1,2,3,4,5,3}, 3, 6, 0);
+int main(void) {
+    int arr[] = {1, 2, 3, 4, 5, 3};
+    int times, index, count;
+    recursive(arr, 3, 6, &index, &count);
+
+    if (index != -1) {
+        printf("The target value was found at index %d and it appears %d times.\n", index, count);
+    } else {
+        printf("The target value was not found in the array.\n");
+    }
+
+    return 0;
 }
