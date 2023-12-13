53d2c2ada0f27a36e1d7f643c43a507a5c177636
HalfPast9ine <116215830+HalfPast9@users.noreply.github.com>
2023-12-12 21:08:57 -0500

    yooo

--- 4. Data Types/main.c
+++ 4. Data Types/main.c
@@ -5,7 +5,12 @@ int main()
 {
     
     int age = 40;
+    float pi = 3.14;
     double gpa = 3.6;
+    complex e = 2.71828;
+    bool isMale = true;
+
+
     char grade = 'D';
     char phrase[] = 'WOOOOO';
 
